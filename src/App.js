3afea586e2ce4db6f8fb7f8e236e13369716d9f2
dineshraj100 <dineshrajmech01@gmail.com>
2022-01-28 12:42:08 +0530

    first time code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
+  const name = [
+    {
+      name: "Dineshraj",
+      image:
+        "https://w0.peakpx.com/wallpaper/670/410/HD-wallpaper-light-yagami-art-facial-expression-anime-deathnote-anime-deathnote-deathnote-l-kira.jpg"
+    },
+
+    {
+      name: "Joker",
+      image:
+        "https://www.lovesove.com/wp-content/uploads/2021/06/Batman-Joker-Dp-Lovesove.jpg"
+    },
+
+    {
+      name: "Yoriichi Tsugikuni",
+      image:
+        "https://i.pinimg.com/564x/4f/bc/34/4fbc34afb99c85e548cd18022fcb272e.jpg"
+    }
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {name.map(({ name, image }) => (
+        <Name name={name} image={image} />
+      ))}
     </div>
   );
 }
-
-export default App;
+function Name({ name, image }) {
+  return (
+    <div className="name">
+      <br></br>
+      <img src={image} width="300" />
+      <h2> Hello sir!! This is {name} Here!!!😀</h2>
+    </div>
+  );
+}
